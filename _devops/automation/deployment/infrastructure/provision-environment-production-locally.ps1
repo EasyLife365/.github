335090368a524a0ef057e134ad4875422177600b
Patrick Lamber <pla@easylife365.cloud>
2026-09-16 02:18:39 +0200

    Updates subscription

--- _devops/automation/deployment/infrastructure/provision-environment-production-locally.ps1
+++ _devops/automation/deployment/infrastructure/provision-environment-production-locally.ps1
@@ -3,6 +3,6 @@ az login
 $root = git rev-parse --show-toplevel
 Set-Location "$root/_devops/automation/deployment/infrastructure"
 
-$subscriptionId = "<production-subscription-id>" # TODO: set to the real subscription this Foundry account deploys into
+$subscriptionId = "f167305a-9bcf-4209-a0af-b3372439a6cb" # TODO: set to the real subscription this Foundry account deploys into
 
 ./provision-environment.ps1 $subscriptionId
